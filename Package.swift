05d51c803c557b4468960fbb19109d7c2231716f
davidmoore1 <david_moore1@sil.org>
2025-09-15 12:25:20 -0400

    Creating release for v0.18.2

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let release = "v0.18.1"
-let frameworks = ["DeviceKit": "20db57af9549d9878fc2ebc5a608910864f66706377d1898c85d1fe0c0d4fd1b", "KeymanEngine": "b152ae8d20d8d28b8ef95af15828e1e3609583a91856522ad3ec56f17863d976", "ReachabilitySIL": "d4ae14de744f80c47e0bff6a092374a721d4a2e56eb776a1497bba5cd7ee1d1c", "Sentry": "72b735748cde04adebdfb3fc7b12dcfcf7a3f5d1d2c6e3d3750115f837c8b9dc", "SentrySwiftUI": "853a15adf96ca643d26d0b056d326bc65091d3533cf29202aaea859f46cc149e", "ZIPFoundation": "a0c775db5a85f2e052d20133ec948852025ddc61f34586f2c1143ce64d4d38fd"]
+let release = "v0.18.2"
+let frameworks = ["DeviceKit": "9ab6fefbc8b43757f5498874eec2bb4ea582618750e61d851137c03e619c6b76", "KeymanEngine": "3dce6a7bfb6127c599766c03aa7094e5c939d3a1d80024c52d0b3490a00d5be6", "ReachabilitySIL": "a11904c79014b288e7327214e8e2c73264ef41d2d086e7aca3838c2aa271a27d", "Sentry": "2a02f543b7d45086d610a9dcb40b20534656f3f11b4cb74dbee2ea94ca84dfde", "SentrySwiftUI": "10cd20ef305d1b4251534e4ec45e36906d811130202851dad0bbdaee5e6820c7", "ZIPFoundation": "e124ab217d79a4f334ab9f0265b34c63f5a19e90100729a5edf1bfc23ce0724c"]
 
 func xcframework(_ package: Dictionary<String, String>.Element) -> Target {
     let url = "https://github.com/davidmoore1/keymanengine-spm/releases/download/\(release)/\(package.key).xcframework.zip"
